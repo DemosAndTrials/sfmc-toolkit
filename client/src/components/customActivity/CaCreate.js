29c87338b1d2659e7de819478fc4d04d644f5021
Ros Haitovich <matrostik@gmail.com>
2019-02-11 20:56:08 +0200

    custom activity section init

---
+++ client/src/components/customActivity/CaCreate.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Title from '../partials/Title';
+
+const CaCreate = () => {
+  return (
+    <Title text="Create Custom Activity"/>
+  );
+};
+
+export default CaCreate;
---
+++ client/src/components/customActivity/CaList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Title from '../partials/Title';
+
+const CaList = () => {
+  return (
+    <Title text="List Custom Activity"/>
+  );
+};
+
+export default CaList;
